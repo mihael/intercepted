73a2c556c56a93467b1f04eb731ad674d97b4be8
mihael <kitschmaster@gmail.com>
2023-05-15 14:14:36 +0200

    Headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,7 @@ require_relative "support/interceptor"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Interceptor
 
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :chrome_headless, screen_size: [1400, 1400]
 
   def after_setup
     start_intercepting
