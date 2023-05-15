2106fa56ef91412bfda89b16c87fd20da34e4b4f
mihael <kitschmaster@gmail.com>
2023-05-15 14:20:43 +0200

    Headless

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,7 +6,14 @@ require_relative "support/interceptor"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Interceptor
 
-  driven_by :selenium, using: :chrome_headless, screen_size: [1400, 1400]
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless=new')
+
+  driver = Capybara.register_driver :chrome_headless do |app|
+    Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: [options])
+  end
+
+  driven_by :chrome_headless
 
   def after_setup
     start_intercepting
